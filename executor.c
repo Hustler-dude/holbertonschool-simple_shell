13b814f6df21615d9332b19553582dd37a908ef4
Hustler-dude <nickhustler12@gmail.com>
2026-04-23 20:07:28 +0200

    Fix task 2: remove access() pre-check, let execve handle errors

--- executor.c
+++ executor.c
@@ -6,18 +6,13 @@
  * @prog_name: shell program name used in error messages
  * @env: environment variables passed to execve
  *
- * Return: exit status of child, 127 if not found, 1 on fork error
+ * Return: exit status of child, 127 if execve fails, 1 on fork error
  */
 int execute(char **args, char *prog_name, char **env)
 {
 	pid_t pid;
 	int status;
 
-	if (access(args[0], X_OK) != 0)
-	{
-		print_error(prog_name, args[0]);
-		return (127);
-	}
 	pid = fork();
 	if (pid == -1)
 	{
@@ -26,16 +21,10 @@ int execute(char **args, char *prog_name, char **env)
 	}
 	if (pid == 0)
 	{
-		if (execve(args[0], args, env) == -1)
-		{
-			print_error(prog_name, args[0]);
-			exit(127);
-		}
+		execve(args[0], args, env);
+		print_error(prog_name, args[0]);
+		exit(127);
 	}
-	else
-	{
-		wait(&status);
-		return (WEXITSTATUS(status));
-	}
-	return (0);
+	wait(&status);
+	return (WEXITSTATUS(status));
 }
